4a66b38f7e8f18154110edf79dcaf33d3e73ff17
Katerina Komarkova <katerina.komarkova@hotmail.com>
2021-06-06 11:22:42 +0200

    Add project files.

---
+++ FirstMobileBlazorBindingsApp/TodoItems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstMobileBlazorBindingsApp
+{
+    public class TodoItems
+    {
+        public bool IsDone { get; set; }
+        public string Text { get; set; }
+
+    }
+}
